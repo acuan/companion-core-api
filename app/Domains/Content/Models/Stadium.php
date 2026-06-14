ff8298a1fa35fdec7a4b24b8c3094d320056dcdf
cuan-integraconsultoria <acuan@integraconsultoria.com.mx>
2026-06-14 00:04:18 -0600

    sumir modelos

--- app/Domains/Content/Models/Stadium.php
+++ app/Domains/Content/Models/Stadium.php
@@ -3,8 +3,35 @@
 namespace App\Domains\Content\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Stadium extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+
+        'provider',
+
+        'external_id',
+
+        'name',
+
+        'fifa_name',
+
+        'city',
+
+        'country',
+
+        'capacity',
+
+        'region',
+    ];
+
+    protected $casts = [
+
+        'external_id' => 'string',
+
+        'capacity' => 'integer',
+    ];
 }
--- app/Domains/Content/Models/Team.php
+++ app/Domains/Content/Models/Team.php
@@ -3,8 +3,24 @@
 namespace App\Domains\Content\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Team extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+
+        'provider',
+        'external_id',
+        'name',
+        'fifa_code',
+        'iso2',
+        'group_code',
+        'flag_url',
+    ];
+
+    protected $casts = [
+        'external_id' => 'string',
+    ];
 }
